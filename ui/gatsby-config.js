a33ca665e0ecb023260d46ba7e73fce75d1262e3
RANDRIAMILASOA Hajaniaina Stanislas <hajaniainastanislas@gmail.com>
2020-09-14 08:14:40 +0300

    Change metadata

--- ui/gatsby-config.js
+++ ui/gatsby-config.js
@@ -1,8 +1,15 @@
 module.exports = {
+  siteMetadata: {
+    title: `SmartPredict | Showcase`,
+    description: `SmartPredict Data Science & Machine Learning showcase`,
+    author: `@SmartPredict`,
+    image:
+      "https://res.cloudinary.com/raslasarslas/image/upload/v1579606346/meta-image_qskzgi.png",
+  },
   plugins: [
-    'gatsby-plugin-top-layout',
+    "gatsby-plugin-top-layout",
     {
-      resolve: 'gatsby-plugin-material-ui',
+      resolve: "gatsby-plugin-material-ui",
       // If you want to use styled components you should change the injection order.
       options: {
         // stylesProvider: {
@@ -12,9 +19,9 @@ module.exports = {
     },
     // If you want to use styled components you should add the plugin here.
     // 'gatsby-plugin-styled-components',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
   ],
   siteMetadata: {
-    title: 'My page',
+    title: "My page",
   },
 };
